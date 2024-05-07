3dd954a6c7a9b6541c579fd3b93eacb3776a2bdb
emifaded <emifadedmusic@gmail.com>
2024-05-07 12:21:32 +0800

    init.sh

---
+++ python/init.sh
@@ -0,0 +1,9 @@
+# initialise everything
+
+./buildrandomtimetables 4 # adjust number for timetables amount
+
+./assignports.sh adjacency station-server.sh
+
+./makewebpage.sh station-server.sh mywebpage.html
+
+# then run ./station-server.sh to start the server
